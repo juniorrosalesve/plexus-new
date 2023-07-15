ca8366cbaed17e64c1a3cebbcfb18a53ac6828cd
Júnior Rosales <74562615+juniorrosalesve@users.noreply.github.com>
2023-07-15 14:08:18 -0500

    finalizando home

--- src/index.php
+++ src/index.php
@@ -4,32 +4,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plexus Business Solutions</title>
-    <link rel="stylesheet" href="./css/styles.css">
-    <link rel="stylesheet" href="./css/extra.css">
+    <link rel="stylesheet" href="./resources/css/styles.css">
+    <link rel="stylesheet" href="./resources/css/extra.css">
+    <link rel="stylesheet" href="./resources/css/swiper.min.css">
+
+    <style>
+        .swiper {
+            width: 100%;
+        }
+
+        .swiper-slide {
+            text-align: center;
+            font-size: 18px;
+            background: #fff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .swiper-slide img {
+            display: block;
+            width: 100%;
+            object-fit: cover;
+        }
+    </style>
 </head>
 <body class="bg-plexus">
-    <table border="0" align="center" cellpadding="0" cellspacing="0">
-        <tr>
-            <td>
-                <div class="container">
-                    <!-- HEADER -->
-                    <?php include_once("./theme/header.php"); ?>
+    <div class="w-[1160px] mx-auto">
+        <!-- HEADER -->
+        <?php include_once("./theme/header.php"); ?>
 
-                    <!-- SLIDER -->
-                    <div class="h-96 bg-gray-400">
-                        <p class="text-center">Slider</p>
-                    </div>
+        <!-- SLIDER -->
+        <div class="swiper sliders">
+            <div class="swiper-wrapper" style="max-height:450px;">
+                <?php 
+                    for($i = 1; $i <= 6; $i++) {
+                        $slider     =   '<div class="swiper-slide">';
+                        $slider     .=  '<img src="./resources/sliders/0'.$i.'.jpg" alt="slider-0'.$i.'"></div>';
+                        echo $slider;
+                    }
+                ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+        <img src="./resources/asegure_negocio.png" alt="asegure-su-negocio">
+        <div class="grid grid-cols-4 bg-white">
+            <?php
+                for($i = 1; $i <= 4; $i++)
+                    echo '<div class="p-1"><img src="./resources/sections/'.$i.'.jpg" alt="sections'.$i.'" class="rounded" style="border:4px solid #0f284c;"></div>';
+            ?>
+        </div>
 
-                    <!-- BODY -->
-                    <div class="h-96 bg-gray-300">
-                        <p class="text-center">Body</p>
-                    </div>
-
-                    <!-- FOOTER -->
-                    <?php include_once("./theme/footer.php"); ?>
+        <!-- BODY -->
+        <div class="bg-white">
+            <p class="p-5 text-lg">Son ya muchos los casos de éxito que avalan nuestra experiencia y resultados.</p>
+        
+            <div class="swiper cases">
+                <div class="swiper-wrapper">
+                    <?php 
+                        for($i = 1; $i <= 5; $i++) 
+                            echo '<div class="swiper-slide"><img src="./resources/cases/'.$i.'.jpg" alt="case-'.$i.'" /></div>';
+                    ?>
                 </div>
-            </td>
-        </tr>
-    </table>
+            </div>
+            <div class="flex justify-around p-10">
+                <div>
+                    <img src="./resources/20_years.png" alt="20-years" class="w-full" />
+                </div>
+                <div class="w-[60%] p-10">
+                    <p style="font-size:20px;color:#4d4d4d;" class="italic">Contamos con <span class="font-bold">más de 20 años de experiencia.</span> Son muchos los casos de éxito que acumulamos en nuestro haber, generando clientes altamente exitosos y satisfechos. Nuestros resultados y retornos exceden las expectativas de nuestros clientes.</p>
+                </div>
+            </div>
+            <div class="mt-1">
+                <img src="./resources/offices.jpg" alt="offices">
+            </div>
+        </div>
+
+        <!-- FOOTER -->
+        <?php include_once("./theme/footer.php"); ?>
+    </div>
+
+    <!-- Scripts -->
+    <script src="./resources/js/swiper-bundle.min.js"></script>
+    <script>
+        var sliders = new Swiper(".sliders", {
+            // loop:true,
+            // autoplay: {
+            //     delay:3000
+            // },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+        var cases = new Swiper(".cases", {});
+    </script>
 </body>
 </html>
--- src/theme/header.php
+++ src/theme/header.php
@@ -1,5 +1,5 @@
-<div style="background-color: #e6e6e6;" class="h-6">
-    <div class="flex float-right text-sm mt-[1px]">
+<div style="background-color: #d9d9d9;" class="h-6">
+    <div class="flex float-right text-xs mt-[3px]">
         <div>
             <a href="#">Ingresa a nuestro equipo</a>
         </div>
@@ -9,18 +9,18 @@
             <a href="#">Español</a>
         </div>
         <div class="ml-3 mr-3">|</div>
-        <div class="mr-2">
+        <div class="mr-8">
             <img src="./resources/flags/en.png" alt="flag-en" class="-mt-1 inline cursor-pointer">
             <a href="#">English</a>
         </div>
     </div>
 </div>
-<header class="bg-white p-5 w-full">
-    <nav class="flex w-full">
+<header class="bg-white p-5 w-full head-border">
+    <nav class="flex w-full mb-5">
         <div class="flex-none">
-            <img src="./resources/logo-plexus.png" alt="logo" class="w-[250px] cursor-pointer">
+            <img src="./resources/logo-plexus.png" alt="logo" class="w-[230px] cursor-pointer">
         </div>
-        <div class="grow ml-4 mt-4">
+        <div class="grow ml-7 mt-3">
             <ul class="inline-block navbar">
                 <li class="home" style="color:white;">Inicio</li>
                 <li class="us">Conócenos</li>
